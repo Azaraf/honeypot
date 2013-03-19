6b34a6cf7f300e2aaa3a3bb4343ed87a6b493ad4
09025 <09025@SKCC09N00429.SKCC.NET>
2013-03-19 02:14:03 -0300

    Porys

--- HoneyPot/gen/skccc/fresh/honeypot/R.java
+++ HoneyPot/gen/skccc/fresh/honeypot/R.java
@@ -25,12 +25,13 @@ public final class R {
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f080000;
-        public static final int action_settings=0x7f080009;
+        public static final int action_settings=0x7f08000a;
         public static final int btn_backup=0x7f080005;
         public static final int btn_config=0x7f080006;
         public static final int btn_donation=0x7f080008;
         public static final int btn_help=0x7f080007;
         public static final int btn_restore=0x7f080004;
+        public static final int checkBox1=0x7f080009;
         public static final int imageView1=0x7f080003;
         public static final int progressBar1=0x7f080001;
         public static final int textView1=0x7f080002;
@@ -39,6 +40,7 @@ public final class R {
         public static final int activity_backup1=0x7f030000;
         public static final int activity_intro=0x7f030001;
         public static final int activity_main=0x7f030002;
+        public static final int listview_backup=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
