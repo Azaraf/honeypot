b0ed7f6119c055f5b3a026fb9553e2709e8d5ba9
09025 <09025@SKCC09N00429.SKCC.NET>
2013-03-19 02:20:39 -0300

    weaf

--- HoneyPot/gen/skccc/fresh/honeypot/R.java
+++ HoneyPot/gen/skccc/fresh/honeypot/R.java
@@ -25,16 +25,17 @@ public final class R {
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f080000;
-        public static final int action_settings=0x7f08000a;
-        public static final int btn_backup=0x7f080005;
-        public static final int btn_config=0x7f080006;
-        public static final int btn_donation=0x7f080008;
-        public static final int btn_help=0x7f080007;
-        public static final int btn_restore=0x7f080004;
-        public static final int checkBox1=0x7f080009;
-        public static final int imageView1=0x7f080003;
-        public static final int progressBar1=0x7f080001;
-        public static final int textView1=0x7f080002;
+        public static final int action_settings=0x7f08000b;
+        public static final int btn_backup=0x7f080006;
+        public static final int btn_config=0x7f080007;
+        public static final int btn_donation=0x7f080009;
+        public static final int btn_help=0x7f080008;
+        public static final int btn_restore=0x7f080005;
+        public static final int checkBox1=0x7f08000a;
+        public static final int imageView1=0x7f080004;
+        public static final int listView1=0x7f080001;
+        public static final int progressBar1=0x7f080002;
+        public static final int textView1=0x7f080003;
     }
     public static final class layout {
         public static final int activity_backup1=0x7f030000;
