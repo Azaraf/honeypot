c94a678208388b8d5c5207488dd4e43ef74d54fa
09025 <09025@SKCC09N00429.SKCC.NET>
2013-03-19 01:57:29 -0300

    TestCommit

--- HoneyPot/gen/skccc/fresh/honeypot/R.java
+++ HoneyPot/gen/skccc/fresh/honeypot/R.java
@@ -36,8 +36,9 @@ public final class R {
         public static final int textView1=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_intro=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_backup1=0x7f030000;
+        public static final int activity_intro=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
